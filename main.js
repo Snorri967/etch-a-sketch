a60d68a8cf3bac53a2b95161ace2881cca40050b
Snorri967 <84525118+Snorri967@users.noreply.github.com>
2023-01-20 03:58:54 +0000

    Update CSS

--- main.js
+++ main.js
@@ -1,42 +1,46 @@
-const title = document.createElement("h1");
-title.classList.add("title");
-title.textContent = "Etch-a-Sketch!";
-document.body.appendChild(title);
-
 const gridContainer = document.createElement("div");
-gridContainer.classList.add("container");
+gridContainer.classList.add("gridContainer");
 document.body.appendChild(gridContainer);
 
-const circleDecoration1 = document.createElement("div");
-circleDecoration1.classList.add("circle1");
-document.body.appendChild(circleDecoration1);
+const container = document.createElement("div");
+container.classList.add("container");
+document.body.appendChild(container);
 
-const circleDecoration2 = document.createElement("div");
-circleDecoration2.classList.add("circle2");
-document.body.appendChild(circleDecoration2);
+const buttonContainer = document.createElement("div")
+buttonContainer.classList.add("buttonContainer");
+container.appendChild(buttonContainer);
 
 const blackPenButton = document.createElement("button");
 blackPenButton.classList.add("blackPenButton");
 blackPenButton.textContent = "Black"; 
 blackPenButton.disabled = true;
-document.body.appendChild(blackPenButton);
+buttonContainer.appendChild(blackPenButton);
 
 const resetButton = document.createElement("button");
 resetButton.classList.add("resetButton");
 resetButton.textContent = "Reset";
-document.body.appendChild(resetButton);
+buttonContainer.appendChild(resetButton)
 
 const multiColorPenButton = document.createElement("button");
 multiColorPenButton.classList.add("multiColorPenButton");
 multiColorPenButton.textContent = "Multi";
 multiColorPenButton.disabled = "true";
-document.body.appendChild(multiColorPenButton);
+buttonContainer.appendChild(multiColorPenButton);
+
+const titleContainer = document.createElement("div");
+titleContainer.classList.add("titleContainer");
+container.appendChild(titleContainer);
+
+const title = document.createElement("h1");
+title.classList.add("title");
+title.textContent = "Sketch-a-Etch!";
+titleContainer.appendChild(title);
 
 function chooseGridSize() {
   const gridSizeButton = document.createElement("button");
   gridSizeButton.classList.add("gridSizeButton");
   gridSizeButton.textContent = "Size";
-  document.body.appendChild(gridSizeButton);
+  buttonContainer.appendChild(gridSizeButton);
   gridSizeButton.addEventListener("click", () => {
     let gridSize = +prompt("Enter a size for your grids, no bigger than 100 or 0 or less!");
     if (gridSize > 100 || gridSize <= 0) {
